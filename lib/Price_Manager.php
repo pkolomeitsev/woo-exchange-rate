8e004a8546c85dfc7d340b983b2a9298dcb147fa
Pavel Kolomeitsev <p.kolomeitsev@gmail.com>
2017-04-14 01:48:35 +0300

    Fixed variation product price for cart

--- lib/Price_Manager.php
+++ lib/Price_Manager.php
@@ -12,8 +12,12 @@ class Price_Manager {
 
         $self = new self();
 
-        add_filter('woocommerce_get_price', array($self, 'get_price'), 9999, 2);
-        add_filter('woocommerce_get_regular_price', array($self, 'get_regular_price'), 9999, 2);
+        add_filter('woocommerce_product_get_price', array($self, 'get_price'), 9999, 2);
+        add_filter('woocommerce_product_get_regular_price', array($self, 'get_regular_price'), 9999, 2);
+        add_filter('woocommerce_product_get_sale_price', array($self, 'get_sale_price'), 9999, 2);
+        add_filter('woocommerce_product_variation_get_price', array($self, 'variation_get_price'), 9999, 2);
+//        add_filter('woocommerce_product_variation_get_regular_price', array($self, 'variation_get_regular_price'), 9999, 2);
+//        add_filter('woocommerce_available_variation', array($self, 'available_variation'), 9999, 3);
         add_filter('wc_price', array($self, 'wc_price'), 9999, 3);
         add_filter('woocommerce_variation_prices', array($self, 'variation_prices'), 9999, 4);
         add_filter('woocommerce_price_format', array($self, 'price_format'), 9999, 2);
@@ -26,22 +30,62 @@ class Price_Manager {
      * @return type
      */
     public function get_price($price, $product = null) {
+        if (!$price) {
+            return $price;
+        }
         $precision = get_option('woocommerce_price_num_decimals');
         //set to 1 if no rate
         $rate = Currency_Manager::get_currency_rate() ?: 1;
         $price = round($price * $rate, $precision);
-
+        
         return $price;
     }
     
-    /**
-     * 
-     * @param type $price
-     * @param type $product
-     * @return type
-     */
     public function get_regular_price($price, $product = null) {
         return $this->get_price($price);
+    }
+    
+    public function get_sale_price($price, $product = null) {
+        return $this->get_price($price);
+    }
+    
+    public function variation_get_price($price, $product = null) {
+        return $this->get_price($price);
+    }
+    
+    public function variation_get_regular_price($price, $product = null) {
+        return $this->get_price($price);
+    }
+
+        /**
+     * Fix for dynamic variable prices
+     * @param array $result
+     * @param WC_Product_Variable $product
+     * @param type $variation
+     * @return type
+     */
+    public function available_variation($result, $product, $variation) {
+        $v_price = $this->get_price($variation->get_price());
+        $v_regular_price = $this->get_regular_price($variation->get_regular_price());
+        
+        if ( '' === $variation->get_price() ) {
+			return apply_filters( 'woocommerce_empty_price_html', '', $variation );
+		}
+        
+        if ($variation->is_on_sale()) {
+            $price = wc_format_sale_price(wc_get_price_to_display(
+                        $variation, array('price' => $v_regular_price)), wc_get_price_to_display(
+                        $variation, array('price' => $v_price)))
+                . $variation->get_price_suffix();
+        } else {
+            $price = wc_price(wc_get_price_to_display($variation, array('price' => $v_price))) . $variation->get_price_suffix();
+        }
+        
+        $result['display_price'] = wc_get_price_to_display($variation, array('price' => $v_price));
+        $result['display_regular_price'] = wc_get_price_to_display($variation, array('price' => $v_regular_price));
+        $result['price_html'] = !empty($result['price_html']) ? '<span class="price">' . $price . '</span>' : '';
+
+        return $result;
     }
 
     /**
@@ -62,7 +106,7 @@ class Price_Manager {
             'decimals' => wc_get_price_decimals(),
             'price_format' => get_woocommerce_price_format()
         ))));
-
+        
         $negative = $price < 0;
         //custom changes
         //price already formated, now clear the old format
@@ -127,7 +171,7 @@ class Price_Manager {
                 $price = $this->get_price($price);
             }
         }
-
+        
         return $prices_array;
     }
 }
